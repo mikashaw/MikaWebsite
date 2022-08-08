854b74dfc1f5fa1655affbbe3135d5398279a558
Mika Shaw <mika.m.shaw@gmail.com>
2022-08-08 17:57:54 -0400

    added qualifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,22 @@ const navMenu = document.getElementById('nav-menu'),
     el.addEventListener('click', toggleSkills)
  })
 
+ /* ================= QUALIFICATIONS ================*/ 
+
+ const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=> {
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
