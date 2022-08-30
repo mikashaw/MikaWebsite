defb06fab3065d24c09540ba193fb40b8b9926f2
Mika Shaw <mika.m.shaw@gmail.com>
2022-08-30 12:50:40 -0400

    added light/dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,3 +163,39 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
   }
 
   window.addEventListener('scroll', scrollHeader)
+
+  /* ================== SHOW SCROLL UP =================== */ 
+
+  function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  }
+
+  window.addEventListener('scroll', scrollUp) 
+
+  /* ====================== DARK LIGHT THEME ==================== */ 
+
+  const themeButton = document.getElementById('theme-button') 
+  const darkTheme = 'dark-theme'
+  const iconTheme = 'uil-sun' 
+
+  const selectedTheme = localStorage.getItem('selected-theme')
+  const selectedIcon = localStorage.getItem('selected-icon') 
+
+  const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light' 
+  const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun' 
+
+  if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add': 'remove'](darkTheme) 
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme) 
+  }
+  
+  // Activate / deactive the theme manually with the button 
+
+  themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+  })
